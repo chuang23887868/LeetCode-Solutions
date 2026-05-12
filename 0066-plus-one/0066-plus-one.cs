4b157ac19264c8373091b7e8c47179c3b532c2de
chuang23887868 <chuang23887868@gmail.com>
2026-05-12 16:15:12 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int[] PlusOne(int[] digits) {
+
+        int n = digits.Length;
+
+        
+
+        for(int i = n-1; i >= 0; i--){
+
+            if(digits[n-1] < 9){
+
+            digits[n-1] ++;
+
+            return digits;
+            }
+
+            digits[i] = 0;
+            
+        }
+
+        int[] res = new int[n + 1];
+
+        res[0] = 1;
+        
+        return res;
+
+        
+        
+    }
+}
